cb13d876f7e5b86e929bd65caea23d635a539c92
Jean-Paul Calderone <exarkun@twistedmatrix.com>
2021-05-12 14:40:23 -0400

    Delegate most packaging to gridsync

--- nix/zkapauthorizer.nix
+++
@@ -1,9 +0,0 @@
-let
-  pkgs = import <nixpkgs> {};
-in
-  pkgs.fetchFromGitHub {
-    owner = "PrivateStorageio";
-    repo = "ZKAPAuthorizer";
-    rev = "b2aaa5f82c6d078bc38d0d8ab0e66d7be449caec";
-    sha256 = "0zhnxba3ql8i0jcpbr8vid5rkvrsa7gvvw2465jq5vb10ivwnjkq";
-  }
