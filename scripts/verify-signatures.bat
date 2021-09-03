c0c9dbea2c620b71a50612ea8384a1394575617d
Chris Wood <chris@privatestorage.io>
2021-09-03 14:50:23 -0400

    Remove unneeded scripts
    These are no longer necessary due to upstream changes in Gridsync:
    https://github.com/gridsync/gridsync/pull/380

--- scripts/verify-signatures.bat
+++
@@ -1,4 +0,0 @@
-set PATH=%PATH%;C:\Program Files (x86)\Windows Kits\10\bin\x64;C:\Program Files (x86)\Windows Kits\10\bin\10.0.17763\x64
-signtool verify /v .\dist\PrivateStorage\Tahoe-LAFS\tahoe.exe
-signtool verify /v .\dist\PrivateStorage\PrivateStorage.exe
-signtool verify /v .\dist\PrivateStorage\PrivateStorage-setup.exe
