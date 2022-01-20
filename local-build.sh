ca62e781afd7ae8382fcd15188db4a6d8c680104
Jean-Paul Calderone <exarkun@twistedmatrix.com>
2022-01-20 08:45:25 -0500

    here's how you do all the things

--- local-build.sh
+++ local-build.sh
@@ -1,1 +1,6 @@
-NIV_OVERRIDE_gridsync=/home/exarkun/Work/python/gridsync nix-build -A privatestorage
+export NIV_OVERRIDE_gridsync=/home/exarkun/Work/python/gridsync
+export NIV_OVERRIDE_tahoe_lafs=/home/exarkun/Work/python/tahoe-lafs
+export NIV_OVERRIDE_magic_folder=/home/exarkun/Work/python/magic-folder
+export NIV_OVERRIDE_zkapauthorizer=/home/exarkun/Work/PrivateStorage/ZKAPAuthorizer
+
+nix-build -A desktopclient
